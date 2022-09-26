d161f8a12902f47563888c0612b6a1dbf4b04e1d
tank <tank@certifiedcoderz.com>
2022-09-26 01:17:06 -0400

    good

--- www/scripts/biz_scriptz/config.js
+++ www/scripts/biz_scriptz/config.js
@@ -1,9 +1,9 @@
-BIZ9_MOBILE_VERSION='3.2.2'
-APP_VERSION='1.8.9'
+BIZ9_MOBILE_VERSION='3.3.4'
+APP_VERSION='1.9.1'
 APP_VENDOR='BiZ9 Framework'
 APP_VENDOR_WEBSITE='bossappz.com'
 APP_TITLE='BiZ9-Mobile'
-APP_TITLE_ID='mobilesep24c'
+APP_TITLE_ID='mobile19'
 dLOUD_URL='https://93.bossappz.com/';
 CLOUD_URL='http://localhost:1901/';
 
