531d6da6f0a464aab2fab84ae7a0fb4c5900ce62
tank <tank@certifiedcoderz.com>
2022-10-11 00:24:34 -0400

    good

--- platforms/android/app/src/main/assets/www/scripts/biz_scriptz/config.js
+++
@@ -1,9 +0,0 @@
-BIZ9_MOBILE_VERSION='3.4.8'
-APP_ID='19'
-APP_VERSION='2.1.3'
-APP_VENDOR='BiZ9 Framework'
-APP_VENDOR_WEBSITE='bossappz.com'
-APP_TITLE='BiZ9-Mobile'
-APP_TITLE_ID='mobile-oct3d'
-CLOUD_URL='https://94.bossappz.com/';
-dLOUD_URL='http://localhost:1901/';
--- www/scripts/biz_scriptz/config.js
+++ www/scripts/biz_scriptz/config.js
@@ -1,9 +1,9 @@
-BIZ9_MOBILE_VERSION='3.5.0'
+BIZ9_MOBILE_VERSION='3.5.1'
 APP_ID='19'
-APP_VERSION='2.1.3'
+APP_VERSION='1.9.9'
 APP_VENDOR='BiZ9 Framework'
 APP_VENDOR_WEBSITE='bossappz.com'
 APP_TITLE='BiZ9-Mobile'
-APP_TITLE_ID='mobile-oct3d'
-CLOUD_URL='https://94.bossappz.com/';
+APP_TITLE_ID='mobile-19'
+CLOUD_URL='https://98.bossappz.com/';
 dLOUD_URL='http://localhost:1901/';
