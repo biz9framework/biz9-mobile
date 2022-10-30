a288855ffdd76016c722e50b51970f886dcb2fe4
tank <tank@certifiedcoderz.com>
2022-10-29 21:08:56 -0400

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,4 +1,4 @@
-BIZ9_MOBILE_VERSION='3.5.6'
+BIZ9_MOBILE_VERSION='3.5.7'
 APP_VERSION='2.0.1'
 APP_ID='19'
 APP_VENDOR='BiZ9 Framework'
@@ -8,7 +8,7 @@ APP_TITLE_ID='mobile-19'
 REPO_URL="git@github.com:bossappz/biz9-mobile-stable.git"
 
 CONFIG_ID="io.bossappz.mobile";
-DOWNLOAD_URL="https://94.bossappz.com/uploads"
+DOWNLOAD_URL="https://98.bossappz.com/uploads"
 DEPLOY_IP=3.88.104.76
 DEPLOY_LOC=/home/admin/www/service-94/public/uploads
 AWS_KEY_PEM=~/www/projectz/121/service/other/aws/ec2_key/121.pem
