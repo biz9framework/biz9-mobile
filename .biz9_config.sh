6d76ec167f121d47b85e480561ffa815ec0cf6e9
tank <tank@certifiedcoderz.com>
2022-11-08 09:48:39 -0500

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,5 +1,5 @@
-BIZ9_MOBILE_VERSION='3.5.8'
-APP_VERSION='2.0.3'
+BIZ9_MOBILE_VERSION='3.5.7';
+APP_VERSION='2.0.8'
 APP_ID='19'
 APP_VENDOR='BiZ9 Framework'
 APP_VENDOR_WEBSITE='bossappz.com'
@@ -8,10 +8,10 @@ APP_TITLE_ID='mobile-19'
 REPO_URL="git@github.com:bossappz/biz9-mobile-stable.git"
 
 CONFIG_ID="io.bossappz.mobile";
-DOWNLOAD_URL="https://121.bossappz.com/uploads"
-DEPLOY_IP=3.88.104.76
-DEPLOY_LOC=/home/admin/www/service-121/public/uploads
-AWS_KEY_PEM=~/www/projectz/121/service/other/aws/ec2_key/121.pem
+DOWNLOAD_URL="https://122.bossappz.com/uploads"
+DEPLOY_IP=54.145.138.40
+DEPLOY_LOC=/home/admin/www/service-122/public/uploads
+AWS_KEY_PEM=~/www/projectz/122/service/other/aws/ec2_key/122.pem
 
 KEY_STORE_PASSWORD="12345678"
 APP_RELEASE_AAB="platforms/android/app/build/outputs/bundle/release/app-release.aab"
--- www/scripts/biz_scriptz/config.js
+++ www/scripts/biz_scriptz/config.js
@@ -1,9 +1,9 @@
-BIZ9_MOBILE_VERSION='3.5.8'
+BIZ9_MOBILE_VERSION='3.5.7'
 APP_ID='19'
-APP_VERSION='2.6.8'
+APP_VERSION='2.0.8'
 APP_VENDOR='BiZ9 Framework'
 APP_VENDOR_WEBSITE='bossappz.com'
 APP_TITLE='BiZ9-Mobile'
-APP_TITLE_ID='mobile-99'
+APP_TITLE_ID='mobile-19'
 CLOUD_URL='https://121.bossappz.com/';
 dLOUD_URL='http://localhost:1901/';
