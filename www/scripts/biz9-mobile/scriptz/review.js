24e2461d1d6428fccc5b57eb23e59c8d90aa4eca
tank <tank@certifiedcoderz.com>
2023-04-14 23:52:30 -0400

    good

--- www/scripts/biz9-mobile/scriptz/review.js
+++ www/scripts/biz9-mobile/scriptz/review.js
@@ -31,11 +31,14 @@ function bind_review(item){
             $('#biz_lbl_card_list_review').hide();
         }
         bind_review_add_event();
-        //review and review-end
         //test start --
+        //bind_test();
+        function bind_test(){
+        //review and review-end
         $("#biz_tb_review_name").val(get_id(999)+'_Full Name');
         $("#biz_tb_review_location").val(get_id(999)+'_location');
         $("#biz_tb_review_comment").val(get_id(999)+"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+        }
         //test end --
     }
 }
