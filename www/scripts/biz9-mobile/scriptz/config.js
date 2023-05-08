2c942901cfffb43fe24378b3605d9440bbc2e112
tank <tank@certifiedcoderz.com>
2023-05-08 00:14:52 -0400

    good

--- www/scripts/biz9-mobile/scriptz/config.js
+++ www/scripts/biz9-mobile/scriptz/config.js
@@ -1,7 +1,7 @@
-BIZ9_MOBILE_VERSION='5.2.5'
+BIZ9_MOBILE_VERSION='5.2.6'
 APP_VERSION='5.4.3'
 APP_TITLE='BiZ9-Mobile'
-APP_TITLE_ID='mobile-168'
+APP_TITLE_ID='mobile-may'
 dLOUD_URL='https://service.bossappz.net';
-CLOUD_URL='https://168-service.bossappz.net';
-dLOUD_URL='http://localhost:1901';
+dLOUD_URL='https://168-service.bossappz.net';
+CLOUD_URL='http://localhost:1901';
