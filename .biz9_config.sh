19148e8c7cc8e907b57f7076dbf3aeae6a66c5f8
tank <tank@certifiedcoderz.com>
2023-11-23 23:57:23 -0500

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,5 +1,5 @@
-BIZ9_MOBILE_VERSION='6.5.9'
-APP_VERSION='4.7.8'
+BIZ9_MOBILE_VERSION='6.6.0'
+APP_VERSION='4.7.9'
 APP_TITLE='BiZ9-Mobile'
 APP_TITLE_ID='mobile-march'
 CONFIG_ID="io.bossappz.mobile";
