7dea228c47cce0f836ba71588a4e9304062bbfc7
tank <tank@certifiedcoderz.com>
2023-12-27 20:49:22 -0500

    good

--- .biz9_config.sh
+++ .biz9_config.sh
@@ -1,7 +1,7 @@
-BIZ9_MOBILE_VERSION='7.0.9'
+BIZ9_MOBILE_VERSION='7.1.0'
 PROJECT_ID='19'
 APP_VERSION='1.0.0'
 APP_TITLE='BiZ9-Mobile'
 APP_TITLE_ID='mobile-march'
 CONFIG_ID="io.bossappz.mobile";
-REPO_URL="${BIZ9_GIT_URL}biz9-mobile.git"
+REPO_URL="${BIZ9_GIT_REPO}biz9-mobile.git"
