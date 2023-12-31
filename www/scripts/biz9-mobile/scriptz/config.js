ffdaf4ee4cc14272b405e6594294c00f89635890
tank <tank@certifiedcoderz.com>
2023-12-30 20:41:18 -0500

    updated about and removed tinymce libz.

--- www/scripts/biz9-mobile/scriptz/config.js
+++ www/scripts/biz9-mobile/scriptz/config.js
@@ -1,7 +1,7 @@
-BIZ9_MOBILE_VERSION='7.1.1'
+BIZ9_MOBILE_VERSION='7.1.2'
 APP_VERSION='1.0.0'
 PROJECT_ID='19'
 APP_VENDOR='BiZ9 Framework Mobile'
 APP_TITLE='BiZ9-Mobile'
-APP_TITLE_ID='mobile-19'
-CLOUD_URL="https://bossappz.com";
+APP_TITLE_ID='mobile-266'
+CLOUD_URL="https://mobile.bossappz343.com";
