276df2b25919dd4d37c18b7e621b7e4846f29ec8
tank <tank@certifiedcoderz.com>
2024-01-13 19:46:21 -0500

    updated framework.

--- platforms/android/app/src/main/assets/www/scripts/biz9-mobile/scriptz/config.js
+++ platforms/android/app/src/main/assets/www/scripts/biz9-mobile/scriptz/config.js
@@ -1,7 +1,8 @@
-BIZ9_MOBILE_VERSION='7.0.9'
+BIZ9_MOBILE_VERSION='7.1.6'
 APP_VERSION='1.0.5'
 PROJECT_ID='19'
 APP_VENDOR='BiZ9 Framework Mobile'
-APP_TITLE='BiZ9-Mobile'
+APP_TITLE='BiZ9-Mobile 2'
 APP_TITLE_ID='mobile-266'
+TEST_MODE=false;
 CLOUD_URL="https://mobile.bossappz343.com";
--- www/scripts/biz9-mobile/scriptz/config.js
+++ www/scripts/biz9-mobile/scriptz/config.js
@@ -1,8 +1,8 @@
-BIZ9_MOBILE_VERSION='7.1.7'
+BIZ9_MOBILE_VERSION='7.1.8'
 APP_VERSION='1.0.0'
 PROJECT_ID='19'
 APP_VENDOR='BiZ9 Framework Mobile'
-APP_TITLE='BiZ9-Mobile'
+APP_TITLE='BiZ9-Mobile 2'
 APP_TITLE_ID='mobile-266'
 TEST_MODE=false;
 CLOUD_URL="https://mobile.bossappz343.com";
