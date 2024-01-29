62e71ce9e331d40d28d064a2ae9a7f96aa83a099
tank <tank@certifiedcoderz.com>
2024-01-29 01:02:59 -0500

    Update manifest.json. playstore.

--- www/scripts/biz9-mobile/scriptz/config.js
+++ www/scripts/biz9-mobile/scriptz/config.js
@@ -1,9 +1,9 @@
-BIZ9_MOBILE_VERSION='7.4.2'
+BIZ9_MOBILE_VERSION='7.4.3'
 APP_VERSION='1.0.0'
 PROJECT_ID='19'
 APP_VENDOR='BiZ9 Framework Mobile'
 APP_TITLE='BiZ9-Mobile'
 APP_TITLE_ID='mobile-19'
-TEST_MODE="true";
+TEST_MODE='true';
 //CLOUD_URL="https://mobile.bossappz343.com";
 CLOUD_URL="http://localhost:1901";
