89c4d70b60115927db341445521c21b0f849a4f5
tank <tank@certifiedcoderz.com>
2024-06-07 12:34:39 -0400

    good

--- www/scripts/biz9-mobile/scriptz/config.js
+++ www/scripts/biz9-mobile/scriptz/config.js
@@ -1,16 +1,16 @@
-BIZ9_MOBILE_VERSION='8.1.8'
+BIZ9_MOBILE_VERSION='8.1.9'
 APP_VERSION='1.0.0'
 PROJECT_ID='19'
 APP_VENDOR='BiZ9 Framework Mobile'
 APP_TITLE='May 21'
 APP_TITLE_ID='mobile-may21'
 TEST_MODE='true';
-//CLOUD_URL="https://mobile.bossappz343.com";
-CLOUD_URL="http://localhost:1901";
+CLOUD_URL="https://mobile.bossappz343.com";
+//CLOUD_URL="http://localhost:1901";
 
 /*
 //266
-BIZ9_MOBILE_VERSION='8.1.8'
+BIZ9_MOBILE_VERSION='8.1.9'
 APP_VERSION='1.0.0'
 PROJECT_ID='19'
 APP_VENDOR='BiZ9 Framework Mobile'
